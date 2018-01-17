cebfc9f2dcef9ff6b3cae4019389c110993fe685
Benjamin Dehli <benjamin@arkitektum.no>
2018-01-17 21:05:51 +0100

    Remove unused code

--- src/components/modals/AddEffect.vue
+++ src/components/modals/AddEffect.vue
@@ -1,25 +1,3 @@
-<!--<template>
-  <div class="modal">
-
-   <p>Add pedal:</p>
-        <label>
-          Manufacturer:
-          <select v-model="$parent.$parent.newEffect.manufacturer">
-            <option v-bind:value="manufacturer" v-for="manufacturer in $parent.$parent.availableEffects.manufacturers">
-              {{ manufacturer.name }}
-            </option>
-          </select>
-        </label>
-        <label>
-          Model:
-          <select v-model="$parent.$parent.newEffect.selectedEffect">
-            <option v-bind:value="effect" v-for="effect in $parent.$parent.newEffect.manufacturer.effects">{{ effect.model }}</option>
-          </select>
-        </label>
-        <button v-bind:disabled="$parent.$parent.disableAddPedalButton" v-on:click="$parent.$parent.addEffect">Add pedal</button>
- </div>
-</template>-->
-
 <script>
 import AddEffectTemplate from "./AddEffectTemplate.quark";
 
@@ -31,11 +9,6 @@ export default {
     }
   },
   template: AddEffectTemplate,
-  props: [],
-  mounted: function () {
-  },
-  watch: {
-  },
   computed: {
     effectsOptionList: function () {
       let effectsOptionList = [];
@@ -51,16 +24,11 @@ export default {
     }
   },
   methods: {
-    addEffect: function () {
-      console.log("adding effect");
-    },
     getEffectById: function (){
       let indexes = this.selectedEffectId.split("-");
       let manufacturerIndex = indexes[0];
       let effectIndex = indexes[1];
       let selectedEffect = this.$parent.$parent.availableEffects.manufacturers[manufacturerIndex].effects[effectIndex];
-      console.log(indexes);
-      console.log(selectedEffect);
       this.$parent.$parent.selectedEffects.push(selectedEffect);
     }
   }
