b569f77e4a5beceabccd8d495fa50fab0f839804
Benjamin Dehli <benjamin@arkitektum.no>
2018-01-19 23:34:23 +0100

    Add basic boilerplate pages for home and instruments

---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h1>Home</h1>
+   
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+	data () {
+		return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/pages/Instruments.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<h1>Instruments</h1>
+    <ul>
+      <li v-for="instrument in instruments">
+        <router-link v-bind:to="'/instruments/' + instrument.id">{{ instrument.name }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Instruments',
+	data () {
+		return {
+      instruments: [
+      {id: '0', name: 'Hohner Clavinet'},
+      {id: '1', name: 'Hammond C3'},
+      {id: '2', name: 'Fender Rhodes'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
