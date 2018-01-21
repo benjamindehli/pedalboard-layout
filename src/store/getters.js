dd1f3bd82978d2f5f005318070c264e14704478d
Benjamin Dehli <benjamin@arkitektum.no>
2018-01-21 19:26:37 +0100

    Add Vuex getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,2 +1,44 @@
 export default {
+	effects(state) {
+		return state.effects;
+	},
+	effectManufacturers(state) {
+		return state.effectManufacturers;
+	},
+	effectManufacturerFromSlug(state, getters) {
+		return slug => state.effectManufacturers.find(effectManufacturer => {
+			return getters.slug(effectManufacturer) == slug;
+		})
+	},
+	effectsFromManufacturer(state) {
+		return manufacturer => state.effects.filter(effect => {
+			return effect.manufacturer == manufacturer;
+		})
+	},
+	effectFromSlugs(state, getters) {
+		return slugs => state.effects.find(effect => {
+			let effectManufacturer = getters.effectManufacturerFromSlug(slugs.effectManufacturer);
+			return effect.manufacturer == effectManufacturer && getters.slug(effect.model) == slugs.effectModel;
+		})
+	},
+	slug(state) {
+		return url => {
+			let slug = url;
+			/* Remove unwanted characters, only accept alphanumeric, space and dash */
+			slug = slug.replace(/[^A-Za-z0-9 -]/g,'');
+
+			/* Replace multi spaces with a single space */
+			slug = slug.replace(/\s{2,}/g,' ');
+
+			/* Replace space with a '-' symbol */
+			slug = slug.replace(/\s/g, "-");
+
+			/* Use lowercase */
+			slug = slug.toLowerCase();
+
+			return slug;
+		}
+		
+		
+	}
 };
